c05db1ab9f5b189b6707bda8a94837a2df161989
ochafik <ochafik@google.com>
2023-11-27 01:13:54 +0000

    Basic support for optional keys

--- grammars/from-json-schema.py
+++ grammars/from-json-schema.py
@@ -1,92 +1,84 @@
+# Generates a GBNF grammar from a JSON schema (https://www.schemastore.org/json/)
 #
-# python grammars/from-json-schema.py https://json.schemastore.org/compile-commands.json
+#   python grammars/from-json-schema.py https://json.schemastore.org/compile-commands.json
 # 
+# Pro-tip: Use projects such as typescript-json-schema to generate a JSON schema from a TypeScript type:
 #
-import json, sys, os, pathlib, re
-from jsonargparse import CLI
+#   echo "export type Foo = { instruction: string, input?: string, output: string }" > foo.d.ts 
+#   python grammars/from-json-schema.py <( npx typescript-json-schema --defaultProps --required foo.d.ts Foo )
+import json, sys, requests, jsonargparse
 
-refs = {}
+schemas = {}
 
 def get_schema(url):
-  if url not in refs:
-    if url.startswith("https://"):
-      import requests
-      schema = requests.get(url).text
+  if url not in schemas:
+    if url.startswith("https://"): 
+      text = requests.get(url).text
     else:
       with open(url, 'rt') as f:
-        schema = f.read()
-    schema = json.loads(schema)
-    definitions = schema.get('definitions', None)
-    if definitions is not None:
-      for k, v in definitions.items():
-        refs[f'#/definitions/{k}'] = v
-    refs[url] = schema
+        text = f.read()
+    schema = json.loads(text)
+    schemas[url] = schema
+    for k, v in schema.get('definitions', {}).items():
+      schemas[f'#/definitions/{k}'] = v
   
-  return refs[url]
+  return schemas[url]
 
 def process_type(schema):
   ref = schema.get('$ref', None)
   if ref is not None:
     yield from process_type(get_schema(ref))
-    return
-
-  otype = schema.get('type', 'object')
-  if otype in ('number', 'integer', 'string', 'boolean'):
-    yield otype
-    return
-
-  if otype == 'array':
-    items = schema.get('items')
-    min_items = schema.get('minItems', 0)
-    if min_items == 0:
+  else:
+    otype = schema.get('type', 'object')
+    if otype in ('number', 'integer', 'string', 'boolean'):
+      yield otype
+    elif otype == 'array':
+      items = schema.get('items')
+      min_items = schema.get('minItems', 0)
       repetition = '*'
-    elif min_items == 1:
-      repetition = '+'
+      if min_items == 1: repetition = '+'
+      elif min_items > 1: print("min_items > 1 not supported yet", file=sys.stderr)
+      yield from ['"["', "(", *process_type(items), '","', ")", repetition, '"]"']
     else:
-      print("min_items > 1 not supported yet", file=sys.stderr)
-      repetition = '*'
+      assert otype == 'object', f"Unknown type {otype}: {schema}"
+      properties = schema['properties']
+      required = schema.get('required', None) or []
+      optional = [k for k in properties.keys() if k not in required]
+      
+      if schema.get('uniqueItems', False): print("uniqueItems not supported yet", file=sys.stderr)
 
-    yield from ['"["', "(", *process_type(items), '","', ")", repetition, '"]"']
-    return
+      def gen_kv(k): return [f'"\\"{k}\\":"', *process_type(properties[k])]
+      
+      yield '"{"'
+      for i, k in enumerate(required):
+        if i > 0: yield '","'
+        yield from gen_kv(k)
 
-  assert otype == 'object', f"Unknown type {otype}: {schema}"
+      yield "("
+      if len(required) > 0: yield from ['","', "("]
 
-  properties = schema['properties']
-  required = schema.get('required', None) or []
-  
-  if schema.get('uniqueItems', False):
-    print("uniqueItems not supported yet", file=sys.stderr)
+      for i in range(len(optional)):
+        [first, *rest] = optional[i:]
+        if i > 0: yield "|"
+        yield from gen_kv(first)
+        for other in rest: yield from ["(", '","', *gen_kv(other), ")?"]
 
-  yield '"{"'
-  for k in required:
-    yield from [f'"\\"{k}\\":"', *process_type(properties[k]), '","']
-
-  for k, v in properties.items():
-    if k not in required:
-      yield from ['(', f'"\\"{k}\\":"', *process_type(v), '","', ')', '?']
-
-  yield '"}"'
+      if len(required) > 0: yield ")"
+      yield from [")?", '"}"']
 
 def main(schema: str):
   schema = get_schema(schema)
   print('\nroot ::= ' + ' '.join(process_type(schema)) + '''
-
 value  ::= object | array | string | number | ("true" | "false" | "null")
-
 object ::= "{" ( string ":" value ("," string ":" value)* )? "}"
-
 array  ::= "[" ( value ("," value)* )? "]"
-
-string ::=
-  "\\"" (
-    [^"\\\\] |
-    "\\\\" (["\\\\/bfnrt] | "u" [0-9a-fA-F] [0-9a-fA-F] [0-9a-fA-F] [0-9a-fA-F]) # escapes
-  )* "\\""
-
+string ::= "\\"" (
+  [^"\\\\] |
+  "\\\\" (["\\\\/bfnrt] | "u" [0-9a-fA-F] [0-9a-fA-F] [0-9a-fA-F] [0-9a-fA-F]) # escapes
+)* "\\""
 integer ::= "-"? ([0-9] | [1-9] [0-9]*)
-
 number ::= integer ("." [0-9]+)? ([eE] [-+]? [0-9]+)?
 ''')
 
 if __name__ == "__main__":
-  CLI(main)
+  jsonargparse.CLI(main)
