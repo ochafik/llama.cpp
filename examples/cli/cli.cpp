eab0170397bd394b7a70ca4b262ff0dace272b78
Olivier Chafik <ochafik@google.com>
2024-05-13 23:32:54 +0100

    cli: urls for info

--- examples/cli/cli.cpp
+++ examples/cli/cli.cpp
@@ -1,4 +1,5 @@
 #include <map>
+#include <regex>
 #include <stdio.h>
 #include <string>
 
@@ -21,51 +22,58 @@ int main(int argc, char ** argv) {
         struct Command {
             bool is_core;
             std::string description;
+            std::string url;
             entry_point main;
         };
         std::map<std::string, Command> commands;
 
-        #define REGISTER_COMMAND(name, symbol, is_core, description) \
+        auto get_url = [&](const std::string & symbol) {
+            return std::string("https://github.com/ggerganov/llama.cpp/tree/master/examples/")
+                + std::regex_replace(symbol, std::regex("_"), "-");
+        };
+
+        #define REGISTER_COMMAND(name, symbol, is_core, description, url) \
             int symbol ## _main(int argc, char ** argv); \
-            commands[name] = {is_core, description, symbol ## _main};
+            commands[name] = {is_core, description, strlen(url) == 0 ? get_url(#symbol) : url, symbol ## _main};
 
         // Core commands
-        commands["commands"] = {true, "List all available commands", nullptr};
-        REGISTER_COMMAND("embed",          embedding, true, "Embedding mode");
-        REGISTER_COMMAND("llava",          llava_cli, true, "");
-        REGISTER_COMMAND("quantize",       quantize, true, "");
-        REGISTER_COMMAND("run",            main, true, "Run a model in chat mode");
-        REGISTER_COMMAND("serve",          server, true, "Serves a model on http://localhost:8080 (web interface + OpenAI-compatible endpoint)");
-        REGISTER_COMMAND("bench",          llama_bench, true, "");
+        commands["commands"] = {true, "List all available commands", "", nullptr};
+
+        REGISTER_COMMAND("embed",          embedding, true, "Embedding mode", "");
+        REGISTER_COMMAND("llava",          llava_cli, true, "Performs generation with LLaVA (Large Language-and-Vision Assistant)", "");
+        REGISTER_COMMAND("quantize",       quantize, true, "Quantizes a model", "");
+        REGISTER_COMMAND("run",            main, true, "Run a model in chat mode", "");
+        REGISTER_COMMAND("serve",          server, true, "Serves a model on http://localhost:8080 (Web interface + OpenAI-compatible endpoint)", "");
+        REGISTER_COMMAND("bench",          llama_bench, true, "Performance testing tool for llama.cpp", "");
 
         // Other commands
-        REGISTER_COMMAND("baby",           baby_llama, false, "");
-        REGISTER_COMMAND("batched",        batched, false, "");
-        REGISTER_COMMAND("beam-search",    beam_search, false, "");
-        REGISTER_COMMAND("bench-batched",  batched_bench, false, "");
-        REGISTER_COMMAND("bench-matmult",  benchmark, false, "");
-        REGISTER_COMMAND("convert-llama2c",convert_llama2c_to_ggml, false, "");
-        REGISTER_COMMAND("eval_callback",  eval_callback, false, "");
-        REGISTER_COMMAND("export_lora",    export_lora, false, "");
-        REGISTER_COMMAND("finetune",       finetune, false, "");
-        REGISTER_COMMAND("gbnf-validate",  gbnf_validator, false, "");
-        REGISTER_COMMAND("gguf-split",     gguf_split, false, "");
-        REGISTER_COMMAND("gritlm",         gritlm, false, "");
-        REGISTER_COMMAND("imatrix",        imatrix, false, "");
-        REGISTER_COMMAND("infill",         infill, false, "");
-        REGISTER_COMMAND("lookahead",      lookahead, false, "");
-        REGISTER_COMMAND("lookup",         lookup, false, "");
-        REGISTER_COMMAND("lookup-create",  lookup_create, false, "");
-        REGISTER_COMMAND("lookup-merge",   lookup_merge, false, "");
-        REGISTER_COMMAND("lookup-stats",   lookup_stats, false, "");
-        REGISTER_COMMAND("parallel",       parallel, false, "");
-        REGISTER_COMMAND("passkey",        passkey, false, "");
-        REGISTER_COMMAND("perplexity",     perplexity, false, "");
-        REGISTER_COMMAND("quantize-stats", quantize_stats, false, "");
-        REGISTER_COMMAND("retrieval",      retrieval, false, "");
-        REGISTER_COMMAND("speculate",      speculative, false, "");
-        REGISTER_COMMAND("tokenize",       tokenize, false, "");
-        REGISTER_COMMAND("train",          train_text_from_scratch, false, "");
+        REGISTER_COMMAND("batched",        batched, false, "Demo of batched generation from a given prompt", "");
+        REGISTER_COMMAND("beam-search",    beam_search, false, "Performs beam search decoding (see https://github.com/ggerganov/llama.cpp/pull/2267)", "");
+        REGISTER_COMMAND("bench-batched",  batched_bench, false, "Benchmark the batched decoding performance of `llama.cpp`", "");
+        REGISTER_COMMAND("bench-matmult",  benchmark, false, "Benmark matrix multiplication performance", "");
+        REGISTER_COMMAND("convert-llama2c",convert_llama2c_to_ggml, false, "Convert llama2.c weights to GGUF", "");
+        REGISTER_COMMAND("eval-callback",  eval_callback, false, "Prints to the console all operations and tensor data (demonstrates inference callbacks usage).", "");
+        REGISTER_COMMAND("export-lora",    export_lora, false, "Applies LORA adapters to a base model and exports the resulting model.", "");
+        REGISTER_COMMAND("finetune",       finetune, false, "Fine-tines a LORA adapter", "");
+        REGISTER_COMMAND("gbnf-validate",  gbnf_validator, false, "Validates an text against a given grammar", "");
+        REGISTER_COMMAND("gguf-split",     gguf_split, false, "CLI to split / merge GGUF files", "");
+        REGISTER_COMMAND("gritlm",         gritlm, false, "Generative Representational Instruction Tuning (GRIT) Example", "");
+        REGISTER_COMMAND("imatrix",        imatrix, false, "Compute an importance matrix for a model and given text dataset", "");
+        REGISTER_COMMAND("infill",         infill, false, "Demo of infill mode with Code Llama models", "");
+        REGISTER_COMMAND("lookahead",      lookahead, false, "Demo of lookahead decoding technique", "");
+        REGISTER_COMMAND("lookup",         lookup, false, "Demo of Prompt Lookup Decoding", "");
+        REGISTER_COMMAND("lookup-create",  lookup_create, false, "", "");
+        REGISTER_COMMAND("lookup-merge",   lookup_merge, false, "", "");
+        REGISTER_COMMAND("lookup-stats",   lookup_stats, false, "", "");
+        REGISTER_COMMAND("parallel",       parallel, false, "Simplified simulation of serving incoming requests in parallel", "");
+        REGISTER_COMMAND("passkey",        passkey, false, "Tests a model's abiltiy to find a needle 'passkey' in a haystack", "https://github.com/ggerganov/llama.cpp/pull/3856");
+        REGISTER_COMMAND("perplexity",     perplexity, false, "Calculates the so-called perplexity value of a language model over a given text corpus.", "");
+        REGISTER_COMMAND("quantize-stats", quantize_stats, false, "Prints quantization statistics for a model", "");
+        REGISTER_COMMAND("retrieval",      retrieval, false, "Demo of simple retrieval technique based on cosine similarity", "");
+        REGISTER_COMMAND("speculate",      speculative, false, "Demo of speculative decoding and tree-based speculative decoding techniques", "");
+        REGISTER_COMMAND("tokenize",       tokenize, false, "Tokenizes a prompt", "");
+        REGISTER_COMMAND("train",          train_text_from_scratch, false, "Train a text model from scratch", "");
+        REGISTER_COMMAND("train-baby",     baby_llama, false, "Example of training a baby llama model", "https://github.com/ggerganov/llama.cpp/pull/1360");
 
         auto print_commands = [&](bool core_only) {
             std::string program_name(argv[0]);
@@ -78,7 +86,7 @@ int main(int argc, char ** argv) {
             fprintf(stderr, "Usage: %s <command> <command args>\n\n", program_name.c_str());
 
             auto print_command = [&](const std::string & name, const Command & command) {
-                fprintf(stderr, "  %s: %s\n", name.c_str(), command.description.c_str());
+                fprintf(stderr, "  %s:\n    %s\n    See %s\n", name.c_str(), command.description.c_str(), command.url.c_str());
             };
 
             fprintf(stderr, "Core commands:\n");
