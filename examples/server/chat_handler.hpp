af2b80953ecf0e77a36688667b6db43f2e56aad4
ochafik <ochafik@google.com>
2024-09-10 04:02:25 +0100

    Update chat_handler.hpp

--- examples/server/chat_handler.hpp
+++ examples/server/chat_handler.hpp
@@ -16,14 +16,33 @@
 #include <random>
 #include <regex>
 
+static std::string _llama_token_to_piece(const struct llama_model * model, llama_token token, bool special) {
+    std::string piece;
+    piece.resize(piece.capacity());  // using string internal cache, 15 bytes + '\n'
+    const int n_chars = llama_token_to_piece(model, token, &piece[0], piece.size(), 0, special);
+    if (n_chars < 0) {
+        piece.resize(-n_chars);
+        int check = llama_token_to_piece(model, token, &piece[0], piece.size(), 0, special);
+        GGML_ASSERT(check == -n_chars);
+    }
+    else {
+        piece.resize(n_chars);
+    }
+
+    return piece;
+}
+
 class ChatHandler {
     using json = nlohmann::ordered_json;
     
     std::string name_;
     json model_context;
-    std::unique_ptr<TemplateNode> chat_template_;
-    std::unique_ptr<TemplateNode> tool_call_system_template_;
-    std::unique_ptr<TemplateNode> tool_call_grammar_template_;
+    std::unique_ptr<jinja::TemplateNode> chat_prologue_;
+    std::unique_ptr<jinja::TemplateNode> chat_template_;
+    std::unique_ptr<jinja::TemplateNode> chat_epilogue_;
+
+    std::unique_ptr<jinja::TemplateNode> tool_call_system_template_;
+    std::unique_ptr<jinja::TemplateNode> tool_call_grammar_template_;
     std::vector<std::string> stop_words_;
 
 public:
@@ -35,51 +54,94 @@ public:
 
     ChatHandler(const std::string & name, const json & model_context, const json & handler) : name_(name), model_context(model_context)
     {
+        std::string chat_prologue;
         std::string chat_template = model_context["tokenizer.chat_template"];
-        std::string tool_call_system_template;
-        std::string tool_call_grammar_template;
+        std::string chat_epilogue;
+
+        std::string eos_token = model_context["eos_token"];
+        std::string bos_token = model_context["bos_token"];
+        // std::string tool_call_system_template;
+        // std::string tool_call_grammar_template;
         
+        if (handler.contains("chat_prologue")) chat_prologue = handler["chat_prologue"];
         if (handler.contains("chat_template")) chat_template = handler["chat_template"];
-        if (handler.contains("tool_call_system_template")) tool_call_system_template = handler["tool_call_system_template"];
-        if (handler.contains("tool_call_grammar_template")) tool_call_grammar_template = handler["tool_call_grammar_template"];
+        if (handler.contains("chat_epilogue")) chat_epilogue = handler["chat_epilogue"];
+
+        // if (handler.contains("tool_call_system_template")) tool_call_system_template = handler["tool_call_system_template"];
+        // if (handler.contains("tool_call_grammar_template")) tool_call_grammar_template = handler["tool_call_grammar_template"];
         
-        if (!chat_template.empty()) chat_template_ = JinjaParser::parse(chat_template);
-        if (!tool_call_system_template.empty()) tool_call_system_template_ = JinjaParser::parse(tool_call_system_template);
-        if (!tool_call_grammar_template.empty()) tool_call_grammar_template_ = JinjaParser::parse(tool_call_grammar_template);
+        if (!chat_prologue.empty()) chat_prologue_ = jinja::Parser::parse(chat_prologue);
+        if (!chat_template.empty()) chat_template_ = jinja::Parser::parse(chat_template);
+        if (!chat_epilogue.empty()) chat_epilogue_ = jinja::Parser::parse(chat_epilogue);
+
+        // if (!tool_call_system_template.empty()) tool_call_system_template_ = jinja::Parser::parse(tool_call_system_template);
+        // if (!tool_call_grammar_template.empty()) tool_call_grammar_template_ = jinja::Parser::parse(tool_call_grammar_template);
     }
 
     const std::string & name() const { return name_; }
 
-    std::pair<std::string, std::string> format_chat(const json & messages, const json & tools, const json & json_schema) const {
+    // std::pair<std::string, std::string> format_chat(const json & messages, const json & tools, const json & json_schema, const std::string & grammar) const {
+    void handle(jinja::Value & values) const {
         std::string grammar;
 
-        auto context_json = json({
-            {"model", model_context},
-            {"tools", tools},
-            {"json_schema", json_schema},
-        });
-        // TODO: cache the grammar & the extra system message based on  the context_json so far (all but messages)
-        if (tool_call_grammar_template_) {
-            Value grammar_context(context_json);
-            grammar = tool_call_grammar_template_->render(grammar_context);
-        }
-        std::string extra_system_message;
-        if (tool_call_system_template_) {
-            Value system_context(context_json);
-            auto system_message = tool_call_system_template_->render(system_context);
-            return {system_message, grammar};
-        }
-        if (extra_system_message.empty()) {
-            context_json["messages"] = messages;
-        } else {
-            auto copy = messages;
-            copy.insert(messages.begin(), {{"role", "system"}, {"content", extra_system_message}});
-            context_json["messages"] = copy;
-        }
-        Value prompt_context(context_json);
-        auto prompt = chat_template_->render(prompt_context);
+        auto context = jinja::Value::context(values);
+        context.set("add_generation_prompt", true);
+        context.set("eos_token", model_context["eos_token"]);
+        context.set("bos_token", model_context["bos_token"]);
 
-        return {prompt, grammar};
+        // jinja::Value context = jinja::Value::context(json({
+        //     // {"model", model_context},
+        //     {"tools", tools},
+        //     {"add_generation_prompt", true},
+        //     {"eos_token", model_context["eos_token"]},
+        //     {"bos_token", model_context["bos_token"]},
+
+        //     {"json_schema", json_schema},
+        //     {"grammar", grammar},
+        // }));
+        if (chat_prologue_) {
+            auto output = chat_prologue_->render(context);
+            // LOG_INFO("chat_prologue", {{"output", output}});
+        }
+
+        // TODO: cache the grammar & the extra system message based on  the context_json so far (all but messages)
+        // context.set("messages", messages);
+        // if (tool_call_grammar_template_) {
+        //     Value grammar_context(context_json);
+        //     grammar = tool_call_grammar_template_->render(grammar_context);
+        // }
+
+        // std::string extra_system_message;
+        // if (tool_call_system_template_) {
+        //     Value system_context(context_json);
+        //     auto system_message = tool_call_system_template_->render(system_context);
+        //     return {system_message, grammar};
+        // }
+        // if (extra_system_message.empty()) {
+        //     context_json["messages"] = messages;
+        // } else {
+        //     auto copy = messages;
+        //     copy.insert(messages.begin(), {{"role", "system"}, {"content", extra_system_message}});
+        //     context_json["messages"] = copy;
+        // }
+        // Value prompt_context(context_json);
+        auto prompt = chat_template_->render(context);
+
+        if (chat_epilogue_) {
+            context.set("prompt", prompt);
+            auto output = chat_epilogue_->render(context);
+            // LOG_INFO("chat_epilogue", {{"output", output}});
+
+            if (context.contains("grammar")) {
+                grammar = context.get<std::string>("grammar", "");
+            }
+        }
+
+        static std::vector<std::string> output_keys {"stop", "grammar", "prompt"};
+        for (const auto & key : output_keys) {
+            if (context.contains(key))
+                values.set(key, context.at(key));
+        }
     }
 
     static json build_model_context(llama_model * model) {
@@ -107,6 +169,9 @@ public:
                 }
             }
         }
+        model_context["eos_token"] = _llama_token_to_piece(model, llama_token_eos(model), /* special= */ true);
+        model_context["bos_token"] = _llama_token_to_piece(model, llama_token_bos(model), /* special= */ true);
+
         return model_context;
     }
 
@@ -140,8 +205,8 @@ public:
                 const auto & handler = handlers[i];
                 if (!handler.contains("condition")) continue;
 
-                auto condition_eval = JinjaParser::parse(handler["condition"]);
-                Value context(json({{"model", model_context}}));
+                auto condition_eval = jinja::Parser::parse(handler["condition"]);
+                jinja::Value context(json({{"model", model_context}}));
                 auto result = string_strip(condition_eval->render(context));
                 if ((result != "True") && (result != "False")) {
                     throw std::runtime_error("Invalid tool call condition evaluation result (expected True/False): " + result);
