20c41ae27e7edc4105074b1b628e2374a8f46578
ochafik <ochafik@google.com>
2025-02-01 21:32:03 +0000

    Update test-mcp.cpp

--- tests/test-mcp.cpp
+++ tests/test-mcp.cpp
@@ -7,12 +7,14 @@ int main()
     //   e.g. python test_server.py
     // that does JSON-RPC style responses on stdout.
     auto subprocess = SingleThreadedJSONRPCStdioSubprocess::create(
-      "/opt/homebrew/Caskroom/miniforge/base/envs/ai/bin/python",
-      // "python",
+      // "/opt/homebrew/Caskroom/miniforge/base/envs/ai/bin/python",
+      "python",
       { "../test_server.py" });
 
     // Make a call
     auto result = subprocess->call("myMethod", {{"param1", 42}, {"param2", "hello"}});
     std::cout << "Got response: " << result.dump() << std::endl;
+
+    // echo '{"jsonrpc": "2.0", "id": "1", "method": "tools/call", "params": {"name": "brave_web_search", "arguments": {"query": "Olivier Chafik"}}}' | npx -y @modelcontextprotocol/server-brave-search --help | jq
     return 0;
 }
